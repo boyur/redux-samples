2ea0b4b136b4b4870d89163d2e059d2702a800b0
Artem Boyur <gboyur@gmail.com>
2017-02-23 13:36:06 +0300

    add counter component

---
+++ src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Counter extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>0</h1>
+        <a href="#">Increment</a>
+      </div>
+    )
+  }
+}
