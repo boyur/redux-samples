acc5ea8efdd1f9dc570e5dba6830dff6c03e25ba
Artem Boyur <gboyur@gmail.com>
2017-02-23 15:58:09 +0300

    redux counter ready

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react'
+import store from '../store'
+import {increment} from '../AC'
 
-export default class Counter extends Component {
+class Counter extends Component {
+
+  state = {
+    count: store.getState()
+  };
+
+  componentDidMount() {
+    this.unsubstribe = store.subscribe(this.handleStoreChenge)
+  }
+
+  componentWillUnmount() {
+    this.unsubstribe();
+  }
 
   render() {
     return (
       <div>
-        <h1>0</h1>
-        <a href="#">Increment</a>
+        <h1>{this.state.count}</h1>
+        <a href="#" onClick={this.handleIncrement}>Increment</a>
       </div>
     )
   }
+
+  handleIncrement = ev => {
+    ev.preventDefault();
+    const action = increment();
+    store.dispatch(action);
+  };
+
+  handleStoreChenge = () => {
+    this.setState({
+      count: store.getState()
+    })
+  }
 }
+
+export default Counter;
---
+++ src/reducer/index.js
@@ -0,0 +1,8 @@
+import {INCREMENT} from '../constants';
+
+export default (state, action) => {
+  switch (action.type) {
+    case INCREMENT: return state + 1;
+  }
+  return state;
+}
